3900ba250fe87ae1d07ad6a599bcf132fb688898
Youssef Doss <youssefdoss97@gmail.com>
2023-01-10 19:03:59 -0500

    initial commit

---
+++ routes/shipments.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const express = require("express");
+const { BadRequestError } = require("../expressError");
+const router = new express.Router();
+
+const { shipProduct } = require("../shipItApi");
+
+/** POST /ship
+ *
+ * VShips an order coming from json body:
+ *   { productId, name, addr, zip }
+ *
+ * Returns { shipped: shipId }
+ */
+
+router.post("/", async function (req, res, next) {
+  if (req.body === undefined) {
+    throw new BadRequestError();
+  }
+  const { productId, name, addr, zip } = req.body;
+  const shipId = await shipProduct({ productId, name, addr, zip });
+  return res.json({ shipped: shipId });
+});
+
+
+module.exports = router;
---
+++ routes/shipments.test.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const request = require("supertest");
+const app = require("../app");
+
+
+describe("POST /", function () {
+  test("valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("throws error if empty request body", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send();
+    expect(resp.statusCode).toEqual(400);
+  });
+});
